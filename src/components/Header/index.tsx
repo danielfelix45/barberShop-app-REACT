898d39b010421111949364222e0d05589d6fff44
Daniel Felix <felixdaniel9@gmail.com>
2023-09-28 14:01:10 -0300

    Add Home Page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import {FiLogIn} from 'react-icons/fi';
 export function Header(){
   return(
     <div className="flex justify-center w-full bg-black">
-      <nav className="flex justify-between w-full max-w-7xl mx-auto p-4 md:py-8">
+      <nav className="flex justify-between w-full max-w-7xl mx-auto p-4 md:p-0 md:py-8">
         <div className="flex items-center text-white text-xl md:text-3xl font-bold font-poppins">
           <Link to={'#'}>Barber<span className="text-[#f1cd30]">Shop</span>.</Link>
         </div>
