46a477c4183cee081fa76049ff17489d6ee7edda
Daniel Felix <felixdaniel9@gmail.com>
2023-10-13 21:45:20 -0300

    Add pagination and select

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
-import {useState, useEffect} from 'react';
-import {FiSearch} from 'react-icons/fi'
+import {useState, useEffect, useCallback} from 'react';
+import {FiChevronsLeft, FiChevronsRight, FiSearch} from 'react-icons/fi'
 
 import {collection, query, getDocs, orderBy, where} from 'firebase/firestore';
 import { db } from '../../services/firebaseConnection';
@@ -16,6 +16,16 @@ function Home() {
   const [clients, setClients] = useState<IClientsProps[]>([]);
   const [loading, setLoading] = useState(true);
   const [input, setInput] = useState('');
+
+  // Pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recordsPerPage, setRecordsPerPage] = useState(5);
+  const lastIndex = currentPage * recordsPerPage;
+  const firstIndex = lastIndex - recordsPerPage;
+  const recordsPage = clients.slice(firstIndex, lastIndex);
+  const numberPage = Math.ceil(clients.length / recordsPerPage);
+  const numbers = [...Array(numberPage + 1).keys()].slice(1)
+
 
   useEffect(() => {
     loadClients();
@@ -36,6 +46,7 @@ function Home() {
           email: doc.data().email,
           phone: doc.data().phone,
           uid: doc.data().uid
+
         })
       })
 
@@ -74,14 +85,33 @@ function Home() {
     setClients(clientsList);
   }
 
+  function previewPage(){
+    if(currentPage !== firstIndex){
+      setCurrentPage(currentPage - 1)
+    }else{
+      return;
+    }
+  }
+
+  function nextPage(){
+    if(currentPage !== firstIndex){
+      setCurrentPage(currentPage + 1)
+    }
+  }
+
+  const records = useCallback((e: any) => {
+    setRecordsPerPage(e.target.value)
+    setCurrentPage(1);
+  }, [])
+
   return (
-    <div className="w-full h-screen bg-gray-500 px-2 pb-6">
+    <div className="w-full min-h-screen bg-gray-500 px-2 pb-20">
       <div className="flex flex-col items-center w-full max-w-7xl mx-auto">
         <h2 className="text-3xl md:text-5xl text-black font-poppins font-semibold mt-28 md:mt-40 mb-14 md:mb-16">Lista de CLientes</h2>
 
         <div className='flex w-full items-center justify-center md:justify-end mb-8'>
           <input 
-            className='px-2 py-2 rounded w-72 outline-none' 
+            className='px-2 py-2 rounded w-72 outline-none shadow-md shadow-gray-800' 
             type="text" 
             placeholder='Procurar...'
             value={input}
@@ -92,11 +122,21 @@ function Home() {
           </button>
         </div>
 
+        <div className='w-full justify-start mb-2 pl-2'>
+          <select className='focus:outline-none rounded px-3 py-1 shadow-md shadow-gray-800' onChange={records}>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="50">50
+            </option>
+          </select>
+        </div>
+
         {loading ? (
           <p className='font-semibold font-poppins text-3xl tracking-wider mt-20'>Carregando...</p>
         ) : clients.length > 0 ? (
-          <div className='w-full shadow border rounded-lg overflow-hidden'>
-            <table className="w-full leading-normal">
+          <div className='w-full shadow-md shadow-gray-800 border rounded-lg overflow-hidden'>
+            <table className="w-full leading-normal overflow-scroll">
               <thead>
                 <tr>
                   <th className=" px-4 py-2 border-b-2 border-gray-200 bg-gray-100 font-poppins font-semibold text-base md:text-lg text-left uppercase tracking-widest">Nome</th>
@@ -105,11 +145,11 @@ function Home() {
                 </tr>
               </thead>
               <tbody>
-                {clients.map( client => (
+                {recordsPage.map( client => (
                   <tr key={client.id}>
-                  <td className="py-4 px-4 border-b border-gray-200 bg-white font-dm-sans text-xs md:text-base">{client.name}</td>
-                  <td className="py-4 px-4 border-b border-gray-200 bg-white font-dm-sans text-xs md:text-base">{client.email}</td>
-                  <td className="py-4 px-4 border-b border-gray-200 bg-white font-dm-sans text-xs md:text-base">{client.phone}</td>
+                  <td className="py-3 px-2 md:px-4 border-b border-gray-200 bg-white font-dm-sans text-xs md:text-base">{client.name}</td>
+                  <td className="py-3 px-2 md:px-4 border-b border-gray-200 bg-white font-dm-sans text-xs md:text-base">{client.email}</td>
+                  <td className="py-3 px-2 md:px-4 border-b border-gray-200 bg-white font-dm-sans text-xs md:text-base">{client.phone}</td>
                   </tr>
                 ))}
               </tbody>
@@ -131,6 +171,20 @@ function Home() {
             </div>
           </div>
         )}
+          <div className="flex bg-white rounded-lg font-[Poppins] mt-3 shadow-md shadow-gray-800">
+            <button onClick={previewPage} className="flex items-center  gap-1 h-12 border-2 border-r-0 border-gray-700 px-4 rounded-l-lg hover:bg-gray-500 hover:font-semibold">
+              <FiChevronsLeft size={22} color="#000" />
+            </button>
+            {
+                numbers.map((page, index) => (
+                  <button key={index} onClick={() => setCurrentPage(page)} className={`h-12 border-2 border-r-0 border-gray-700
+                  w-12 ${currentPage === page && 'bg-gray-500 text-black font-semibold'}`}>{page}</button>
+                ))
+            }
+            <button onClick={nextPage} className="flex items-center gap-1 h-12 border-2 border-gray-700 px-4 rounded-r-lg hover:bg-gray-500 hover:font-semibold">
+              <FiChevronsRight size={22} color="#000" />
+            </button>
+          </div>
       </div>
     </div>
   )
